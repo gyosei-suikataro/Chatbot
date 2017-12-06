396eb6f548a6ab1392a4ce51927fefd1c326a97c
sato taro <dsjvvknu@gmail.com>
2017-12-06 15:53:07 +0900

    a

--- src/main/java/com/example/sample/SubUserRepository.java
+++ src/main/java/com/example/sample/SubUserRepository.java
@@ -1,1 +1,13 @@
+package com.example.sample;
 
+import com.example.sample.SubUser;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+
+
+@Repository
+public interface SubUserRepository extends JpaRepository<SubUser, String>{
+
+	
+}
--- src/main/java/com/example/sample/UserService.java
+++ src/main/java/com/example/sample/UserService.java
@@ -6,6 +6,7 @@ import org.springframework.transaction.annotation.Transactional;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 import com.example.sample.User;
+import com.example.sample.SubUser;
 
 
 @Service
@@ -14,13 +15,19 @@ public class UserService {
 	@Autowired
 	private UserRepository repository;
 	
+	@Autowired
+	private SubUserRepository subrepository;
+	
+	
 	private BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
 	
 	
 	@Transactional
     public void registerUser(Integer no,String custid,String custname,String orgname,String password,String effect,String role) {
 		User user = new User(no,custid,custname,orgname,passwordEncoder.encode(password),effect,role);
+		SubUser subuser = new SubUser(custid,password,role);
         repository.save(user);
+        subrepository.save(subuser);
     }
     
     

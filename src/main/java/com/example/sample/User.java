e8e7b31783d028d846758c83fbcb49ba3647dede
sato taro <dsjvvknu@gmail.com>
2017-12-07 17:17:26 +0900

    a

--- src/main/java/com/example/sample/User.java
+++ src/main/java/com/example/sample/User.java
@@ -13,7 +13,7 @@ public class User implements Serializable {
 	private static final long serialVersionUID = 1L;
 
 	@Id
-	@GeneratedValue(GenerationType.AUTO)
+	@GeneratedValue(strategy = GenerationType.AUTO)
 	private Integer no;
 	@Column
 	private String custid;

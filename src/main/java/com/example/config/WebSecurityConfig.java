59e19beadf3241ebb5c27a40fca530dd6df5b2f9
Keitaro Nishizawa <keitaro_nishizawa@gyosei-system.co.jp>
2017-12-08 16:25:32 +0900

    s

--- src/main/java/com/example/config/WebSecurityConfig.java
+++ src/main/java/com/example/config/WebSecurityConfig.java
@@ -32,7 +32,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 	protected void configure(HttpSecurity http) throws Exception {
 		http
 		.authorizeRequests()
-		.antMatchers("/login").permitAll()
+		.antMatchers("/").permitAll()
 		/*
 		.antMatchers("/**").hasAnyAuthority("ADMIN","USER")
 		.antMatchers("/Home").hasAnyAuthority("ADMIN","USER")

1e943a1841c91ba2263c838bc04e35e937abf6e0
sato taro <dsjvvknu@gmail.com>
2017-12-08 11:41:59 +0900

    a

--- src/main/java/com/example/sample/User.java
+++ src/main/java/com/example/sample/User.java
@@ -8,7 +8,7 @@ import javax.persistence.*;
  * 
  */
 @Entity
-@Table(name="custinfo1")
+@Table(name="custinfo")
 public class User implements Serializable {
 	private static final long serialVersionUID = 1L;
 

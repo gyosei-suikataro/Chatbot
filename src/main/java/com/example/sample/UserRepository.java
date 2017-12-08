f7dc036a3247d4b43e8c2c1591187a5a70c43694
Keitaro Nishizawa <keitaro_nishizawa@gyosei-system.co.jp>
2017-12-08 11:39:28 +0900

    s

--- src/main/java/com/example/sample/UserRepository.java
+++ src/main/java/com/example/sample/UserRepository.java
@@ -4,11 +4,8 @@ import com.example.sample.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-
-
 @Repository
 public interface UserRepository extends JpaRepository<User, Integer>{
 
 	
 }
-

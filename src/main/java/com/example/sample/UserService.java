fbaf4a4d0ff87b65a3e61e3138dc8368bb324eca
Keitaro Nishizawa <keitaro_nishizawa@gyosei-system.co.jp>
2017-12-08 13:15:00 +0900

    s

--- src/main/java/com/example/sample/UserService.java
+++ src/main/java/com/example/sample/UserService.java
@@ -6,8 +6,6 @@ import org.springframework.transaction.annotation.Transactional;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 import com.example.sample.User;
-import com.example.sample.SubUser;
-
 
 @Service
 public class UserService {
@@ -15,24 +13,13 @@ public class UserService {
 	@Autowired
 	private UserRepository repository;
 	
-	@Autowired
-	private SubUserRepository subrepository;
-	
-	
 	private BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
-	
-	
+		
 	@Transactional
     public void registerUser(Integer no,String custid,String custname,String orgname,String password,String effect,String role) {
 		User user = new User(no,custid,custname,orgname,passwordEncoder.encode(password),effect,role);
 		//SubUser subuser = new SubUser(custid,passwordEncoder.encode(password),role);
         repository.save(user);
         //subrepository.save(subuser);
-    }
-    
-    
-	
+    }	
 }
-
-
-

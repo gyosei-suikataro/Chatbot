e23754652b65f78ca4bbc62f64ab2a5dcf129e2f
Keitaro Nishizawa <keitaro_nishizawa@gyosei-system.co.jp>
2017-12-08 16:33:49 +0900

    s

--- src/main/java/com/example/config/WebSecurityConfig.java
+++ src/main/java/com/example/config/WebSecurityConfig.java
@@ -44,7 +44,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 		 */
 		.anyRequest().authenticated(); 
 
-		http.formLogin()
+		http.csrf().disable().formLogin()
 		.loginProcessingUrl("processing")
 		.loginPage("/")
 		//.failureHandler(new SampleAuthenticationFailureHandler())

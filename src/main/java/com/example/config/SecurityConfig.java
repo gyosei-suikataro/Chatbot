f5ae37a113d4e36f8b2e59ac7f6de19a0448c879
Keitaro Nishizawa <keitaro_nishizawa@gyosei-system.co.jp>
2017-12-12 13:43:01 +0900

    s

--- src/main/java/com/example/config/SecurityConfig.java
+++ src/main/java/com/example/config/SecurityConfig.java
@@ -67,16 +67,12 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	}
 	 */
 
-	@Configuration
-	public class SecurityConfig extends WebSecurityConfigurerAdapter {
-
-		@Override
-		protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-			auth.inMemoryAuthentication()
-			.withUser("user").password("user").roles("USER")
-			.and()
-			.withUser("admin").password("admin").roles("ADMIN");
-		}
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.inMemoryAuthentication()
+		.withUser("user").password("user").roles("USER")
+		.and()
+		.withUser("admin").password("admin").roles("ADMIN");
 	}
 
 }

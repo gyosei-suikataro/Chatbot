3f23fb09cb76fe3cf7c52c24d40205289b6954ad
Keitaro Nishizawa <keitaro_nishizawa@gyosei-system.co.jp>
2017-12-12 14:06:56 +0900

    s

--- src/main/java/com/example/config/SecurityConfig.java
+++ src/main/java/com/example/config/SecurityConfig.java
@@ -9,22 +9,29 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http
-            .authorizeRequests()
-                .antMatchers("/", "/login", "/login-error").permitAll()
-                .antMatchers("/**").hasRole("USER")
-                .and()
-            .formLogin()
-                .loginPage("/login").failureUrl("/login-error");
-    }
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http
+		.authorizeRequests()
+		.antMatchers("/", "/login", "/login-error").permitAll()
+		.antMatchers("/**").hasRole("USER")
+		.and()
+		.formLogin()
+		.loginPage("/login").failureUrl("/login-error")
+		http.formLogin().loginProcessingUrl("/login").loginPage("/signin")
+		.failureUrl("?error").defaultSuccessUrl("/", false)
+		.usernameParameter("loginId").passwordParameter("password")
+		.and().logout().logoutRequestMatcher(new AntPathRequestMatcher("signout"))
+		.logoutSuccessUrl("/signin")
+		.deleteCookies("JSESSIONID")
+		.invalidateHttpSession(true).permitAll();
+	}
 
-    @Autowired
-    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
-        auth
-            .inMemoryAuthentication()
-                .withUser("user").password("password").roles("USER");
-    }
+	@Autowired
+	public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+		auth
+		.inMemoryAuthentication()
+		.withUser("user").password("password").roles("USER");
+	}
 
 }

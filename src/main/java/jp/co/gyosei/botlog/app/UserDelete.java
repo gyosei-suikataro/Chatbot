0a9e638c292dc41df9ee91fd1c908f323f3ddff7
sato taro <dsjvvknu@gmail.com>
2017-12-15 09:46:36 +0900

    a

--- src/main/java/jp/co/gyosei/botlog/app/UserDelete.java
+++ src/main/java/jp/co/gyosei/botlog/app/UserDelete.java
@@ -1,1 +1,26 @@
+package jp.co.gyosei.botlog.app;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import jp.co.gyosei.botlog.domain.repository.UserRepository;
+import java.util.ArrayList;
+
+@Controller
+@RequestMapping("userlist")
+public class UserDelete{
+	
+	@Autowired
+	UserRepository repository;
+	
+	@PostMapping
+	public String Delete(Integer no) {
+		List<Integer> list = new ArrayList<Integer>();
+		list.add(no);
+		repository.delete(list);
+		
+	}
+	
+}

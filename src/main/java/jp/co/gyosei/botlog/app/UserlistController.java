34aebe7892830e381c887e580d230971333524e8
sato taro <dsjvvknu@gmail.com>
2017-12-18 17:12:37 +0900

    k

--- src/main/java/jp/co/gyosei/botlog/app/UserlistController.java
+++ src/main/java/jp/co/gyosei/botlog/app/UserlistController.java
@@ -17,6 +17,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 */
 //import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PathVariable;
 

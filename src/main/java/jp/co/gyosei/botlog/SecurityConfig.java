4d738ba1b4d07ce8d8fc51d6a50e2ea7c765961f
Keitaro Nishizawa <keitaro_nishizawa@gyosei-system.co.jp>
2017-12-19 10:38:40 +0900

    s

---
+++ src/main/java/jp/co/gyosei/botlog/SecurityConfig.java
@@ -0,0 +1,61 @@
+package jp.co.gyosei.botlog;
+ 
+ import org.springframework.beans.factory.annotation.Autowired;
+ import org.springframework.context.annotation.Bean;
+ import org.springframework.security.authentication.encoding.PasswordEncoder;
+ import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+ import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+ import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+ import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+ 
+ @SuppressWarnings("deprecation")
+ @EnableWebSecurity
+ public class SecurityConfig extends WebSecurityConfigurerAdapter {
+ 
+ 	@Override
+ 	protected void configure(HttpSecurity http) throws Exception {
+ 		http
+ 		.authorizeRequests()
+ 		.antMatchers("/", "/login", "/loginerror").permitAll()
+ 		.antMatchers("/**").hasRole("USER")
+ 		//.antMatchers("/**").hasAnyAuthority("ADMIN","USER")
+ 		.and()
+ 		.formLogin()
+ 		.loginProcessingUrl("/loading")
+ 		.loginPage("/")
+ 		//.failureUrl("?error")
+ 		.defaultSuccessUrl("/signup", true)
+ 		//.usernameParameter("username")
+ 		.usernameParameter("custid")
+ 		.passwordParameter("password");
+ 
+         http
+         .rememberMe()
+         .tokenValiditySeconds(86400);
+ 
+ 		http
+ 		.logout()
+ 		.logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
+ 		.logoutSuccessUrl("/login")
+ 		.deleteCookies("JSESSIONID")
+ 		.invalidateHttpSession(true)
+ 		.permitAll();
+ 
+ 	}
+ /*
+ 	@Autowired
+ 	public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+ 		auth
+ 		.inMemoryAuthentication()
+ 		.withUser("user").password("password").roles("USER");
+ 	}
+ */
+ 	/*	
+     @Bean
+     public BCryptPasswordEncoder passwordEncoder() {
+         return new BCryptPasswordEncoder();
+     }
+ 	 */
+ }

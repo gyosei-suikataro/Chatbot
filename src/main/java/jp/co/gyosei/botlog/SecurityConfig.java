10e4c224cae5f69ef5b8183a4a20b43df50dea2f
Keitaro Nishizawa <keitaro_nishizawa@gyosei-system.co.jp>
2017-12-19 17:32:04 +0900

    s

--- src/main/java/jp/co/gyosei/botlog/SecurityConfig.java
+++ src/main/java/jp/co/gyosei/botlog/SecurityConfig.java
@@ -1,61 +1,73 @@
 package jp.co.gyosei.botlog;
- 
- import org.springframework.beans.factory.annotation.Autowired;
- import org.springframework.context.annotation.Bean;
- import org.springframework.security.authentication.encoding.PasswordEncoder;
- import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
- import org.springframework.security.config.annotation.web.builders.HttpSecurity;
- import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
- import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
- import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
- import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
- 
- @SuppressWarnings("deprecation")
- @EnableWebSecurity
- public class SecurityConfig extends WebSecurityConfigurerAdapter {
- 
- 	@Override
- 	protected void configure(HttpSecurity http) throws Exception {
- 		http
- 		.authorizeRequests()
- 		.antMatchers("/", "/login", "/loginerror").permitAll()
- 		.antMatchers("/**").hasRole("USER")
- 		//.antMatchers("/**").hasAnyAuthority("ADMIN","USER")
- 		.and()
- 		.formLogin()
- 		.loginProcessingUrl("/loading")
- 		.loginPage("/")
- 		//.failureUrl("?error")
- 		.defaultSuccessUrl("/signup", true)
- 		//.usernameParameter("username")
- 		.usernameParameter("custid")
- 		.passwordParameter("password");
- 
-         http
-         .rememberMe()
-         .tokenValiditySeconds(86400);
- 
- 		http
- 		.logout()
- 		.logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
- 		.logoutSuccessUrl("/login")
- 		.deleteCookies("JSESSIONID")
- 		.invalidateHttpSession(true)
- 		.permitAll();
- 
- 	}
- /*
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.authentication.encoding.PasswordEncoder;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+
+@SuppressWarnings("deprecation")
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+	@Autowired
+	private AuthenticationProviderImpl authenticationProvider;
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http
+		.authorizeRequests()
+		.antMatchers("/", "/login", "/loginerror").permitAll()
+		.antMatchers("/**").hasRole("USER")
+		//.antMatchers("/**").hasAnyAuthority("ADMIN","USER")
+		.and()
+		.formLogin()
+		.loginProcessingUrl("/loading")
+		.loginPage("/")
+		//.failureUrl("?error")
+		.defaultSuccessUrl("/signup", true)
+		//.usernameParameter("username")
+		.usernameParameter("custid")
+		.passwordParameter("password");
+
+		http
+		.rememberMe()
+		.tokenValiditySeconds(86400);
+
+		http
+		.logout()
+		.logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
+		.logoutSuccessUrl("/login")
+		.deleteCookies("JSESSIONID")
+		.invalidateHttpSession(true)
+		.permitAll();
+
+	}
+	/*
  	@Autowired
  	public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
  		auth
  		.inMemoryAuthentication()
  		.withUser("user").password("password").roles("USER");
  	}
- */
- 	/*	
+	 */
+	/*	
      @Bean
      public BCryptPasswordEncoder passwordEncoder() {
          return new BCryptPasswordEncoder();
      }
- 	 */
- }
+	 */
+
+	@Autowired
+	public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+		// ì∆é©îFèÿÉNÉâÉXÇê›íËÇ∑ÇÈ
+		auth
+		.authenticationProvider(authenticationProvider);
+		//.userDetailsService(userDetailsService);
+	}
+}
--- src/main/java/jp/co/gyosei/botlog/app/LoginController.java
+++ src/main/java/jp/co/gyosei/botlog/app/LoginController.java
@@ -1,14 +1,25 @@
 package jp.co.gyosei.botlog.app;
 
-
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import jp.co.gyosei.botlog.domain.form.LoginForm;
 
 @Controller
 public class LoginController {
 
-	@RequestMapping({"/","/login"})
-	String login() {
-		return "login";
-	}
+    @RequestMapping(value = "/")
+    public String index(Model model) {
+        model.addAttribute(new LoginForm());
+        return "/login";
+    }
+/*
+    // SpringConfig‚Åİ’è‚µ‚½ƒƒOƒCƒ“‚Å‚«‚È‚©‚Á‚½ê‡‚Ìˆ—‚ğ’è‹`‚·‚é
+    @RequestMapping(value = "/login-error")
+    public String loginError(Model model) {
+        model.addAttribute("loginError", true);
+        return "/login";
+    }
+*/
 }

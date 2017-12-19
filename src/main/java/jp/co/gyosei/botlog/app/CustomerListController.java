8f3453899483556cb416d70e0f61e7f4fe6c41ad
Keitaro Nishizawa <keitaro_nishizawa@gyosei-system.co.jp>
2017-12-19 15:31:29 +0900

    s

--- src/main/java/jp/co/gyosei/botlog/app/CustomerListController.java
+++ src/main/java/jp/co/gyosei/botlog/app/CustomerListController.java
@@ -1,32 +1,16 @@
 package jp.co.gyosei.botlog.app;
 
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RequestBody;
-/*
-import org.springframework.validation.BindingResult;
-import org.springframework.validation.annotation.Validated;
-
-import org.springframework.web.bind.annotation.DeleteMapping;
-*/
-//import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.PathVariable;
 
 import jp.co.gyosei.botlog.domain.entity.CustinfoEntity;
 import jp.co.gyosei.botlog.domain.service.CustinfoService;
 import jp.co.gyosei.botlog.domain.repository.CustinfoRepository;
 
 import java.util.List;
-import java.util.ArrayList;
 
 @Controller
 @RequestMapping("CustomerList")
@@ -37,7 +21,6 @@ public class CustomerListController {
 	
 	@Autowired
 	CustinfoRepository custinfoRepository;
-	
 	
 	@GetMapping
 	public String list(Model model) {
--- src/main/java/jp/co/gyosei/botlog/domain/entity/CustinfoEntity.java
+++ src/main/java/jp/co/gyosei/botlog/domain/entity/CustinfoEntity.java
@@ -97,5 +97,4 @@ public class CustinfoEntity implements Serializable {
 		this.role = role;
 	}
 
-
 }

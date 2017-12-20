1d624a4ca5b961ee75881e1731ab09b9e7483378
Keitaro Nishizawa <keitaro_nishizawa@gyosei-system.co.jp>
2017-12-20 14:13:54 +0900

    s

--- src/main/java/jp/co/gyosei/botlog/SecurityConfig.java
+++ src/main/java/jp/co/gyosei/botlog/SecurityConfig.java
@@ -10,7 +10,6 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 
-@SuppressWarnings("deprecation")
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 

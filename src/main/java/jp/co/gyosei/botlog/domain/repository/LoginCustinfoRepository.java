8733f51d655605a248afdc2ec20751e447badef0
Keitaro Nishizawa <keitaro_nishizawa@gyosei-system.co.jp>
2017-12-20 09:31:35 +0900

    s

--- src/main/java/jp/co/gyosei/botlog/domain/repository/LoginCustinfoRepository.java
+++ src/main/java/jp/co/gyosei/botlog/domain/repository/LoginCustinfoRepository.java
@@ -1,11 +1,11 @@
 package jp.co.gyosei.botlog.domain.repository;
 
-import jp.co.gyosei.botlog.domain.entity.CustinfoEntity;
+import jp.co.gyosei.botlog.domain.entity.LoginCustinfoEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface LoginCustinfoRepository extends JpaRepository<CustinfoEntity, Integer>{
+public interface LoginCustinfoRepository extends JpaRepository<LoginCustinfoEntity, Integer>{
 
 	LoginCustinfoRepository loginCustinfoRepository(String custid, String password);
 	

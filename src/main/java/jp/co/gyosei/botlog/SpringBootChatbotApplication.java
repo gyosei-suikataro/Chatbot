0018d97bde5d5cdb1a8bca8ec8853bb9ba24d42b
tomoaki nagasaka <tomoaki_nagasaka@gyosei-system.co.jp>
2017-12-21 17:04:54 +0900

    更新

--- src/main/java/jp/co/gyosei/botlog/SpringBootChatbotApplication.java
+++ src/main/java/jp/co/gyosei/botlog/SpringBootChatbotApplication.java
@@ -5,10 +5,29 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 //import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 //import org.springframework.context.annotation.ComponentScan;
 
+import com.linecorp.bot.model.event.Event;
+import com.linecorp.bot.model.event.MessageEvent;
+import com.linecorp.bot.model.event.message.TextMessageContent;
+import com.linecorp.bot.model.message.TextMessage;
+import com.linecorp.bot.spring.boot.annotation.EventMapping;
+import com.linecorp.bot.spring.boot.annotation.LineMessageHandler;
+
 
 @SpringBootApplication
+@LineMessageHandler
 public class SpringBootChatbotApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootChatbotApplication.class, args);
 	}
+	
+	@EventMapping
+    public TextMessage handleTextMessageEvent(MessageEvent<TextMessageContent> event) {
+        System.out.println("event: " + event);
+        return new TextMessage(event.getMessage().getText());
+    }
+
+    @EventMapping
+    public void handleDefaultMessageEvent(Event event) {
+        System.out.println("event: " + event);
+    }
 }

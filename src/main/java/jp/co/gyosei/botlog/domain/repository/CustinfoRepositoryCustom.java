48f0b8e0aa49ee1cdecdc5dd9225768b781c8a22
Keitaro Nishizawa <keitaro_nishizawa@gyosei-system.co.jp>
2017-12-21 12:56:13 +0900

    s

--- src/main/java/jp/co/gyosei/botlog/domain/repository/CustinfoRepositoryCustom.java
+++ src/main/java/jp/co/gyosei/botlog/domain/repository/CustinfoRepositoryCustom.java
@@ -9,7 +9,7 @@ import org.springframework.security.core.GrantedAuthority;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface CustinfoRepositoryCustom extends JpaRepository<CustinfoEntity, String>{
+public interface CustinfoRepositoryCustom extends JpaRepository<CustinfoEntity, Integer>{
 
 	CustinfoRepositoryCustom custinfoRepositoryCustom(String custid, String password, Collection<? extends GrantedAuthority> role);
 

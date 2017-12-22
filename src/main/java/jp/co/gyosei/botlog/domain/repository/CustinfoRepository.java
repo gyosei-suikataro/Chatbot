6650eb01bd6ea2d11c31004e3a80e68fc4fdff72
gyosei-suikataro <34331186+gyosei-suikataro@users.noreply.github.com>
2017-12-22 10:20:36 +0900

    s

--- src/main/java/jp/co/gyosei/botlog/domain/repository/CustinfoRepository.java
+++ src/main/java/jp/co/gyosei/botlog/domain/repository/CustinfoRepository.java
@@ -4,12 +4,7 @@ import jp.co.gyosei.botlog.domain.entity.CustinfoEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-
-
-
 @Repository
 public interface CustinfoRepository extends JpaRepository<CustinfoEntity, Integer>{
-	
-	
-}
 
+}

32bb730806b01bc89a81c4262edc06107be3051d
Keitaro Nishizawa <keitaro_nishizawa@gyosei-system.co.jp>
2017-12-22 14:19:16 +0900

    s

--- src/main/java/jp/co/gyosei/botlog/domain/service/CustinfoService.java
+++ src/main/java/jp/co/gyosei/botlog/domain/service/CustinfoService.java
@@ -3,34 +3,22 @@ package jp.co.gyosei.botlog.domain.service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-//import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
-import jp.co.gyosei.botlog.domain.entity.CustinfoEntityImpl;
-import jp.co.gyosei.botlog.domain.repository.CustinfoRepositoryCustom;
+import jp.co.gyosei.botlog.domain.entity.CustinfoEntity;
+import jp.co.gyosei.botlog.domain.repository.CustinfoRepository;
 
 @Service
 public class CustinfoService {
 	
 	@Autowired
-	private CustinfoRepositoryCustom custinfoRepositoryCustom;
-	
-	//private BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+	private CustinfoRepository custinfoRepository;
+
+	private BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
 	
 	@Transactional
-    public void registerCustomer(String custid, String password, String role) {
-		CustinfoEntityImpl custinfoEntityImpl = new CustinfoEntityImpl(custid, password, role);
-		custinfoRepositoryCustom.save(custinfoEntityImpl);
+    public void registerCustomer(Integer no,String custid,String custname,String orgname,String password,String effect,String role) {
+		CustinfoEntity custinfoEntity = new CustinfoEntity(no,custid,custname,orgname,passwordEncoder.encode(password),effect,role);
+		custinfoRepository.save(custinfoEntity);
     }
-	
-	
-	
-	
-    
-	
-    
-    
-	
 }
-
-
-

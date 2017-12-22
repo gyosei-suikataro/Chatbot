4fa5adc998613bab63c3914ff8a4f9539009a118
Keitaro Nishizawa <keitaro_nishizawa@gyosei-system.co.jp>
2017-12-22 10:00:51 +0900

    s

--- src/main/java/jp/co/gyosei/botlog/impl/UserDetailsServiceImpl.java
+++ src/main/java/jp/co/gyosei/botlog/impl/UserDetailsServiceImpl.java
@@ -1,4 +1,4 @@
-package jp.co.gyosei.botlog;
+package jp.co.gyosei.botlog.impl;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;

4b2f290fd5d8b0927c52401517ea91d025d4ee7b
sato taro <dsjvvknu@gmail.com>
2017-12-22 09:30:04 +0900

    a

--- src/main/java/jp/co/gyosei/botlog/app/CustomerListController.java
+++ src/main/java/jp/co/gyosei/botlog/app/CustomerListController.java
@@ -50,9 +50,11 @@ public class CustomerListController {
 	
 	@PostMapping
 	@ResponseStatus(HttpStatus.NO_CONTENT)
-	void delete(@RequestParam(value=rowIds[]) Integer[] rowIds) {
+	public String delete(@RequestParam(value="rowIds[]") Integer[] rowIds) {
 		for (int no: rowIds){
 			custinfoRepository.delete(no);
-		}	
+		}
+		
+		return "CustomerList";
 	}
 }

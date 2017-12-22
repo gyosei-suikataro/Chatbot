ede1bf91cbbe1a5627e2093b045d92931a38419b
Keitaro Nishizawa <keitaro_nishizawa@gyosei-system.co.jp>
2017-12-22 11:41:37 +0900

    s

--- src/main/java/jp/co/gyosei/botlog/app/LoginController.java
+++ src/main/java/jp/co/gyosei/botlog/app/LoginController.java
@@ -8,13 +8,13 @@ import jp.co.gyosei.botlog.domain.form.LoginForm;
 
 @Controller
 public class LoginController {
-
+/*
 	@RequestMapping(value = "/")
 	public String login(Model model) {
 		model.addAttribute(new LoginForm());
 		return "login";
 	}
-	/*
+
     // SpringConfig‚Åİ’è‚µ‚½ƒƒOƒCƒ“‚Å‚«‚È‚©‚Á‚½ê‡‚Ìˆ—‚ğ’è‹`‚·‚é
     @RequestMapping(value = "/login-error")
     public String loginError(Model model) {
@@ -23,6 +23,11 @@ public class LoginController {
     }
 	 */
 
+	@RequestMapping(value = "/login")
+	public String Login() {
+		return "login";
+	}
+	
 	@RequestMapping(value = "/menu")
 	public String Menu() {
 		return "menu";

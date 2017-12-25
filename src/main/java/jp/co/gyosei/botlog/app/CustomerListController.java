e97d51b15e7dd0c73805f76eaa0dd12408c983e9
Keitaro Nishizawa <keitaro_nishizawa@gyosei-system.co.jp>
2017-12-25 11:51:18 +0900

    s

--- src/main/java/jp/co/gyosei/botlog/app/CustomerListController.java
+++ src/main/java/jp/co/gyosei/botlog/app/CustomerListController.java
@@ -3,60 +3,68 @@ package jp.co.gyosei.botlog.app;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMethod;
+/*
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.DeleteMapping;
+*/
+//import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PathVariable;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.validation.Valid;
 
 import jp.co.gyosei.botlog.domain.entity.CustinfoEntity;
-//import jp.co.gyosei.botlog.domain.entity.CustinfoEntityImpl;
+import jp.co.gyosei.botlog.domain.form.CustomerForm;
+import jp.co.gyosei.botlog.domain.form.CustomerListForm;
 import jp.co.gyosei.botlog.domain.service.CustinfoService;
-//import jp.co.gyosei.botlog.domain.service.CustinfoService2;
 import jp.co.gyosei.botlog.domain.repository.CustinfoRepository;
-//import jp.co.gyosei.botlog.domain.repository.CustinfoRepositoryCustom;
 
 import java.util.List;
+import java.util.ArrayList;
+
+
 
 @Controller
-@RequestMapping("CustomerList")
 public class CustomerListController {
 
 	@Autowired
-	CustinfoService custinfoService;
-
+    CustinfoService custinfoService;
+	
 	@Autowired
 	CustinfoRepository custinfoRepository;
-
-	@GetMapping
+	
+	
+	
+	@RequestMapping(value = "CustomerList", method = RequestMethod.GET)
 	public String list(Model model) {
 		List<CustinfoEntity> custinfoEntity = custinfoRepository.findAll();
 		model.addAttribute("custinfoEntity",custinfoEntity);
+		model.addAttribute("customerListForm", new CustomerListForm());
 		return "CustomerList";
 	}
-
-	/*
-	@PostMapping
-	@ResponseBody
-		public void delete(@RequestBody no[] NO) {
-		  repository.delete(NO);
-
-		    }
-	 */
-/*
-	@Controller
-	@RequestMapping("menu")
-	public class CustomerListController2{
-
-		@Autowired
-		CustinfoService2 custinfoService2;
-
-		@Autowired
-		CustinfoRepositoryCustom custinfoRepositoryCustom;
-
-		@GetMapping
-		public String list(Model model1) {
-			List<CustinfoEntityImpl> custinfoEntityImpl = custinfoRepositoryCustom.findAll();
-			model1.addAttribute("custinfoEntityImpl",custinfoEntityImpl);
-			return "menu";
+	
+	
+	@RequestMapping(value = "CustomerList", method = RequestMethod.POST)
+    public String customerPost(Model model, @Valid CustomerListForm customerListForm, BindingResult bindingResult, HttpServletRequest request) {
+		
+		Integer[] rowIdsdata = customerListForm.getRowIds();
+		for(int i = 0; i < rowIdsdata.length; i++) {
+			Integer no = rowIdsdata[i];
+			custinfoRepository.delete(no);
 		}
+		
+		return "Customer";
 	}
-	*/
+	
 }
---
+++ src/main/java/jp/co/gyosei/botlog/domain/form/CustomerListForm.java
@@ -0,0 +1,15 @@
+package jp.co.gyosei.botlog.domain.form;
+
+public class CustomerListForm {
+	
+	 //private Integer[] rowIds = new Integer[] {16,17,18,19};
+	 private Integer[] rowIds;
+	 
+	 public Integer[] getRowIds() {
+	        return rowIds;
+	    }
+	 
+	 public void setRowIds(Integer[] rowIds) {
+	        this.rowIds = rowIds;
+	    }
+}

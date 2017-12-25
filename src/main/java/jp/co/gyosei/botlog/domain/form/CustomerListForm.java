59aee65b4f1f9075dea82601ec4da137034f14e1
Keitaro <keitaro_nishizawa@gyosei-system.co.jp>
2017-12-25 15:50:57 +0900

    s

--- src/main/java/jp/co/gyosei/botlog/domain/form/CustomerListForm.java
+++ src/main/java/jp/co/gyosei/botlog/domain/form/CustomerListForm.java
@@ -3,12 +3,12 @@ package jp.co.gyosei.botlog.domain.form;
 public class CustomerListForm {
 	
 	 //private Integer[] rowIds = new Integer[] {16,17,18,19};
-	 private Integer[] rowIds;
+	 private Integer[]  rowIds;
 	 
 	 public Integer[] getRowIds() {
 	        return rowIds;
 	    }
-	 
+
 	 public void setRowIds(Integer[] rowIds) {
 	        this.rowIds = rowIds;
 	    }

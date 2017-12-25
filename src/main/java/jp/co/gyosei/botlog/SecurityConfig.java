3ff3136c921e24560ea33dc4c0a91891e640d5a5
Keitaro <keitaro_nishizawa@gyosei-system.co.jp>
2017-12-25 15:34:53 +0900

    s

--- src/main/java/jp/co/gyosei/botlog/SecurityConfig.java
+++ src/main/java/jp/co/gyosei/botlog/SecurityConfig.java
@@ -1,12 +1,15 @@
 /*package jp.co.gyosei.botlog;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http
@@ -29,6 +32,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 		.invalidateHttpSession(true)
 		.permitAll();
 	}
+	
 	@Autowired
 	public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
 		auth

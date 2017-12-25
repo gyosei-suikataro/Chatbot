138266c9b3d31994cac0c590c8da6c711477ce34
Keitaro <keitaro_nishizawa@gyosei-system.co.jp>
2017-12-25 15:46:47 +0900

    s

--- src/main/java/jp/co/gyosei/botlog/SecurityConfig.java
+++ src/main/java/jp/co/gyosei/botlog/SecurityConfig.java
@@ -1,43 +1,77 @@
-/*package jp.co.gyosei.botlog;
+package jp.co.gyosei.botlog;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+import jp.co.gyosei.botlog.impl.UserDetailsServiceImpl;
+import jp.co.gyosei.botlog.impl.AuthenticationProviderImpl;
 
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private UserDetailsServiceImpl userDetailsService;
+	
+	@Autowired
+	private AuthenticationProviderImpl authenticationProvider;
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http
 		.authorizeRequests()
-		.antMatchers("/", "/login", "/login-error").permitAll()
-		.antMatchers("/**").hasRole("USER")
+		.antMatchers("/", "/login", "/loginerror").permitAll()
+		//.antMatchers("/**").hasRole("USER")
+		//.antMatchers("/**").hasAnyAuthority("ADMIN","USER")
 		.and()
 		.formLogin()
 		.loginProcessingUrl("/loading")
-		.loginPage("/login")
+		.loginPage("/")
 		//.failureUrl("?error")
-		.defaultSuccessUrl("/signup", true)
+		.defaultSuccessUrl("/Customer", true)
+		//.usernameParameter("username")
 		.usernameParameter("custid")
-		.passwordParameter("password")
-		.and()
+		.passwordParameter("password");
+
+		http
+		.rememberMe()
+		.tokenValiditySeconds(86400); 
+
+		http
 		.logout()
-		.logoutRequestMatcher(new AntPathRequestMatcher("/signout"))
-		.logoutSuccessUrl("/login")
+		.logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
+		.logoutSuccessUrl("/")
 		.deleteCookies("JSESSIONID")
 		.invalidateHttpSession(true)
 		.permitAll();
-	}
 
+	}
+	
+/*	
+ 	@Autowired
+ 	public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+ 		auth
+ 		.inMemoryAuthentication()
+ 		.withUser("user").password("password").roles("USER");
+ 	}
+*/ 
+	
+     @Bean
+     public BCryptPasswordEncoder passwordEncoder() {
+         return new BCryptPasswordEncoder();
+     }
+ 
 	@Autowired
 	public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+		// 独自認証クラスを設定する
 		auth
-		.inMemoryAuthentication()
-		.withUser("user").password("password").roles("USER");
+		.authenticationProvider(authenticationProvider)
+		.userDetailsService(userDetailsService);
 	}
+
 }
-*/

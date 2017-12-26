462ff5aa7ccfefc3e211f28db04ea510c004d585
sato taro <dsjvvknu@gmail.com>
2017-12-26 15:50:13 +0900

    k

--- src/main/java/jp/co/gyosei/botlog/app/CustomerListController.java
+++ src/main/java/jp/co/gyosei/botlog/app/CustomerListController.java
@@ -58,7 +58,7 @@ public class CustomerListController {
 	
 	
 	@RequestMapping(value = "CustomerList", method = RequestMethod.POST)
-    public String customerPost(Model model, @Valid CustomerListForm customerListForm, BindingResult bindingResult, HttpServletRequest request) {
+    public String customerPost(Model model, CustomerListForm customerListForm, BindingResult bindingResult, HttpServletRequest request) {
 		
 		Integer [] rowIdsdata = customerListForm.getRowIds();
 		for(int i = 0; i < rowIdsdata.length; i++) {

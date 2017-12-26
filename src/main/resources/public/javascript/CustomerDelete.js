02fa2b4384fde655f3dcf85e03266ac652b7d046
sato taro <dsjvvknu@gmail.com>
2017-12-26 10:57:10 +0900

    a

--- src/main/resources/public/javascript/CustomerDelete.js
+++ src/main/resources/public/javascript/CustomerDelete.js
@@ -1,2 +1,27 @@
+/*<![CDATA[*/
+var rowIds = [];
 
+$(function() {
+	$("#grid-basic").bootgrid({
+		selection: true, 
+		multiSelect: true,
+		rowSelect: true,
+	    keepSelection: true
+	}).on("selected.rs.jquery.bootgrid", function(e, rows)
+	{
+	    for (var i = 0; i < rows.length; i++)
+	    {
+	        rowIds.push(rows[i].no);
+	    }
+	}).on("deselected.rs.jquery.bootgrid", function(e, rows)
+	{
+		for (var i = 0; i < rows.length; i++)
+	    {
+	    	rowIds.some(function(v, ii){
+	    	    if (v==rows[i].no) rowIds.splice(ii,1);
+	    	}); 
+	    }
+	});
+});
+/*]]>*/
 

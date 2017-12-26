425c7e3840ff8fc075a368c17847ad27eaf4088b
sato taro <dsjvvknu@gmail.com>
2017-12-26 13:52:05 +0900

    a

--- src/main/java/jp/co/gyosei/botlog/domain/form/CustomerListForm.java
+++ src/main/java/jp/co/gyosei/botlog/domain/form/CustomerListForm.java
@@ -4,16 +4,16 @@ package jp.co.gyosei.botlog.domain.form;
 public class CustomerListForm {
 	
 	 //private Integer[] rowIds = new Integer[] {12};
-	 private Integer[] rowIds  = new Integer [] {};
+	 private Integer[] rowIds;
 	 
 	 
 	 public Integer[] getRowIds() {
 	        return rowIds;
 	    }
 	 
-	 /*
+	 
 	 public void setRowIds(Integer[] rowIds) {
 	        this.rowIds = rowIds;
 	    }
-	    */
+	    
 }

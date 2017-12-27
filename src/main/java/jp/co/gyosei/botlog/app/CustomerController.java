62dc3ab852eed3994157df9f1e3f1117f61a1324
sato taro <dsjvvknu@gmail.com>
2017-12-27 14:49:24 +0900

    k

--- src/main/java/jp/co/gyosei/botlog/app/CustomerController.java
+++ src/main/java/jp/co/gyosei/botlog/app/CustomerController.java
@@ -27,20 +27,20 @@ public class CustomerController {
 	@Autowired
 	CustinfoRepository custinfoRepository;
 	
-	/*
+	
 	@RequestMapping("/")
 	public String Home() {
 		return "Customer";
 	}
-	*/
 	
-	@GetMapping("/")
+	
+	@GetMapping("/Customer")
 	public String signup(Model model) {
         model.addAttribute("customerForm", new CustomerForm()); 
         return "Customer";
     }
 	
-	@PostMapping("/")
+	@PostMapping("/Customer")
     public String customerPost(@Valid CustomerForm customerForm, BindingResult bindingResult, HttpServletRequest request,@Valid Model model) {
 		if(bindingResult.hasErrors()) {
 			return "Customer";

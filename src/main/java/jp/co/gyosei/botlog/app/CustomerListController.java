4a6b0679e5630775ed99fcf812c3e1586976cc9a
sato taro <dsjvvknu@gmail.com>
2017-12-27 16:54:10 +0900

    k

--- src/main/java/jp/co/gyosei/botlog/app/CustomerListController.java
+++ src/main/java/jp/co/gyosei/botlog/app/CustomerListController.java
@@ -61,15 +61,13 @@ public class CustomerListController {
 	   public String customerPost(Model model, CustomerListForm customerListForm, BindingResult bindingResult) {
 			
 		Integer [] rowIdsdata = customerListForm.getRowIds();
-		System.out.println(rowIdsdata);
-		System.out.println(Arrays.toString(rowIdsdata));
+		//System.out.println(rowIdsdata);
+		//System.out.println(Arrays.toString(rowIdsdata));
 			
 			for(int i = 0; i < rowIdsdata.length; i++) {
 				Integer no = rowIdsdata[i];
 				custinfoRepository.delete(no);
 			}
-			
-			
 			return "Customer";
 		}
 		

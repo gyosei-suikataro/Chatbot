36acd9ecc23bd56332281d9dcde372d34499c232
Keitaro Nishizawa <keitaro_nishizawa@gyosei-system.co.jp>
2017-12-27 10:47:19 +0900

    s

--- src/main/java/jp/co/gyosei/botlog/SecurityConfig.java
+++ src/main/java/jp/co/gyosei/botlog/SecurityConfig.java
@@ -51,15 +51,6 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 		.permitAll();
 
 	}
-	
-/*	
- 	@Autowired
- 	public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
- 		auth
- 		.inMemoryAuthentication()
- 		.withUser("user").password("password").roles("USER");
- 	}
-*/ 
 	 
 	@Autowired
 	public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {

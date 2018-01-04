68cfbf72ee2894fc3979033c3fe6ee0f9a2a0ca8
Keitaro Nishizawa <keitaro_nishizawa@gyosei-system.co.jp>
2018-01-04 10:58:22 +0900

    s

--- src/main/java/jp/co/gyosei/botlog/impl/UserDetailsServiceImpl.java
+++ src/main/java/jp/co/gyosei/botlog/impl/UserDetailsServiceImpl.java
@@ -1,18 +1,12 @@
 package jp.co.gyosei.botlog.impl;
 
-import java.util.Collection;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.authority.AuthorityUtils;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Component;
 
-import org.springframework.security.core.GrantedAuthority;
-
 import jp.co.gyosei.botlog.LoginCust;
-import jp.co.gyosei.botlog.LoginUserDetails;
 import jp.co.gyosei.botlog.domain.repository.CustinfoRepositoryCustom;
 
 @Component
@@ -30,16 +24,6 @@ public class UserDetailsServiceImpl implements UserDetailsService {
 			throw new UsernameNotFoundException("ユーザーが見つかりませんでした。");
 		}
 
-		LoginCust role = null;
-		return new LoginUserDetails(role, getAuthorities(role));
-	}
-	
-	@SuppressWarnings("unused")
-	private Collection<GrantedAuthority> getAuthorities(LoginCust role){
-		if(role.getAuthorities().equals("ADMIN")){
-			return AuthorityUtils.createAuthorityList("USER","ADMIN");
-		} else {
-			return AuthorityUtils.createAuthorityList("USER");
-		}
+		return new LoginCust(cust);
 	}
 }

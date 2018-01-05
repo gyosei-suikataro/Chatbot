744122794ea994a4f1290d766104d4a97b444f5d
Keitaro Nishizawa <keitaro_nishizawa@gyosei-system.co.jp>
2018-01-05 15:10:28 +0900

    s

--- src/main/java/jp/co/gyosei/botlog/impl/UserDetailsServiceImpl.java
+++ src/main/java/jp/co/gyosei/botlog/impl/UserDetailsServiceImpl.java
@@ -3,39 +3,34 @@ package jp.co.gyosei.botlog.impl;
 import java.util.Collection;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.AuthorityUtils;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Component;
 
-import org.springframework.security.core.GrantedAuthority;
-
-import jp.co.gyosei.botlog.LoginCust;
 import jp.co.gyosei.botlog.LoginUserDetails;
 import jp.co.gyosei.botlog.domain.repository.CustinfoRepositoryCustom;
 
 @Component
 public class UserDetailsServiceImpl implements UserDetailsService {
 
-	@Autowired
-	private CustinfoRepositoryCustom custinfoRepositoryCustom;
+    @Autowired
+    private CustinfoRepositoryCustom custinfoRepositoryCustom;
 
-	@Override
-	public UserDetails loadUserByUsername(String custid)
-			throws UsernameNotFoundException {
+    @Override
+    public LoginUserDetails loadUserByUsername(String custid)
+            throws UsernameNotFoundException {
 
-		CustinfoEntityImpl cust = custinfoRepositoryCustom.findOne(custid);
+    	CustinfoEntityImpl cust = custinfoRepositoryCustom.findOne(custid);
 		if (cust == null) {
-			throw new UsernameNotFoundException("ユーザーが見つかりませんでした。");
-		}
+            throw new UsernameNotFoundException("ユーザーが見つかりませんでした。");
+        }
 
-		LoginCust role = null;
-		return new LoginUserDetails(role, getAuthorities(role));
-	}
-	
+        return new LoginUserDetails(cust);
+    }
 	@SuppressWarnings("unused")
-	private Collection<GrantedAuthority> getAuthorities(LoginCust role){
+	private Collection<GrantedAuthority> getAuthorities(LoginUserDetails role){
 		if(role.getAuthorities().equals("ADMIN")){
 			return AuthorityUtils.createAuthorityList("USER","ADMIN");
 		} else {

99f36cb2ced3042d8504a19d9e1d7b1100be7a35
Keitaro Nishizawa <keitaro_nishizawa@gyosei-system.co.jp>
2018-01-09 11:46:23 +0900

    s

--- src/main/java/jp/co/gyosei/botlog/domain/repository/CustinfoRepositoryCustom.java
+++ src/main/java/jp/co/gyosei/botlog/domain/repository/CustinfoRepositoryCustom.java
@@ -6,12 +6,12 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.stereotype.Repository;
 
-import jp.co.gyosei.botlog.domain.entity.CustinfoEntity;
+import jp.co.gyosei.botlog.impl.CustinfoEntityImpl;
 
 @Repository
-public interface CustinfoRepositoryCustom extends JpaRepository<CustinfoEntity, String>{
+public interface CustinfoRepositoryCustom extends JpaRepository<CustinfoEntityImpl, String>{
 
-	static CustinfoEntity custinfoRepositoryCustom(String custid, String password, Collection<? extends GrantedAuthority> role) {
+	static CustinfoEntityImpl custinfoRepositoryCustom(String custid, String password, Collection<? extends GrantedAuthority> role) {
 		// TODO 自動生成されたメソッド・スタブ
 		return null;
 	}

5fba12841e3e189b399bfe058ce9237287655c19
Keitaro Nishizawa <keitaro_nishizawa@gyosei-system.co.jp>
2018-01-09 18:26:56 +0900

    s

--- src/main/java/jp/co/gyosei/botlog/LoginCust.java
+++ src/main/java/jp/co/gyosei/botlog/LoginCust.java
@@ -11,6 +11,7 @@ import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.User;
 
 import jp.co.gyosei.botlog.domain.entity.CustinfoEntity;
+//import jp.co.gyosei.botlog.domain.entity.CustinfoEntity;
 import jp.co.gyosei.botlog.impl.CustinfoEntityImpl;
 
 @Data
@@ -33,7 +34,7 @@ public class LoginCust extends User {
     	super(cust.custid, cust.password, true, true, true, true, new ArrayList<GrantedAuthority>());
         custid = cust.custid;
         password = cust.password;
-        //role = cust.role;
-        role = "ADMIN";
+        role = cust.role;
+        //role = "ADMIN";
     }
 }

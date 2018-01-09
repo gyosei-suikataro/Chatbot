674b054f65d8acf2bd78b3932cfb3c70dbbf33b6
Keitaro Nishizawa <keitaro_nishizawa@gyosei-system.co.jp>
2018-01-09 16:17:58 +0900

    s

--- src/main/java/jp/co/gyosei/botlog/SecurityConfig.java
+++ src/main/java/jp/co/gyosei/botlog/SecurityConfig.java
@@ -26,7 +26,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 		http
 		.authorizeRequests()
 		.antMatchers("/", "/login", "/loginerror").permitAll()
-		.antMatchers("/**").hasAnyAuthority("ADMIN","USER")
+		//.antMatchers("/**").hasAnyAuthority("ADMIN","USER")
 		.and()
 		.formLogin()
 		.loginProcessingUrl("/loading")

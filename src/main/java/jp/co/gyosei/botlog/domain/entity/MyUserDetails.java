00df207b427161f6543f0612a0389cbdc7dd200e
Keitaro Nishizawa <keitaro_nishizawa@gyosei-system.co.jp>
2018-01-16 16:13:27 +0900

    s

---
+++ src/main/java/jp/co/gyosei/botlog/domain/entity/MyUserDetails.java
@@ -0,0 +1,69 @@
+package jp.co.gyosei.botlog;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+@Data
+@NoArgsConstructor
+@AllArgsConstructor 
+@Entity
+@Table(name="custinfo") 
+public class MyUserDetails implements UserDetails {
+	
+	private static final long serialVersionUID = 1L;
+	@Id 
+	@GeneratedValue 
+	private String custid;
+	@Column(nullable = false) 
+	private String custname; 
+	@Column(nullable = false) 
+	private String password;
+	@Column(nullable = false) 
+	private Boolean effect; 
+	@Column(nullable = false) 
+	private String role;
+	public Collection<GrantedAuthority> getAuthorities() { 
+		List<GrantedAuthority> authorities = new ArrayList<>(); 
+		authorities.add(new SimpleGrantedAuthority(role)); 
+		return authorities; }
+	@Override
+	public String getPassword() {
+		// TODO 自動生成されたメソッド・スタブ
+		return null;
+	}
+	@Override
+	public String getUsername() {
+		// TODO 自動生成されたメソッド・スタブ
+		return null;
+	}
+	@Override
+	public boolean isAccountNonExpired() {
+		// TODO 自動生成されたメソッド・スタブ
+		return false;
+	}
+	@Override
+	public boolean isAccountNonLocked() {
+		// TODO 自動生成されたメソッド・スタブ
+		return false;
+	}
+	@Override
+	public boolean isCredentialsNonExpired() {
+		// TODO 自動生成されたメソッド・スタブ
+		return false;
+	}
+	@Override
+	public boolean isEnabled() {
+		// TODO 自動生成されたメソッド・スタブ
+		return false;
+	}
+}
